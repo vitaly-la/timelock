9359b3a5bcb556c7082648a44a81f47862834911
vitaly-la <148568747+vitaly-la@users.noreply.github.com>
2025-04-07 15:00:51 +0100

    unlocking time in seconds

--- timelock.py
+++ timelock.py
@@ -5,6 +5,7 @@ import hashlib
 import os
 import subprocess
 import sys
+import time
 
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
@@ -15,27 +16,25 @@ from progress.bar import Bar
 HALF_KEY_SIZE = 1024
 
 
-def gen_puzzle(iterations: int) -> tuple[int, int]:
+def gen_puzzle(squarings: int) -> tuple[int, int]:
     p = getPrime(HALF_KEY_SIZE)
     q = getPrime(HALF_KEY_SIZE)
     modulo = p * q
     phi = (p - 1) * (q - 1)
-    secret_key = pow(2, pow(2, iterations, phi), modulo)
+    secret_key = pow(2, pow(2, squarings, phi), modulo)
     return secret_key, modulo
 
 
-def solve_puzzle(iterations: int, modulo: str) -> int:
+def solve_puzzle(squarings: int, modulo: str) -> int:
     proc = subprocess.run(
-        [os.environ["SOLVER"], "2", str(iterations % 100), modulo],
+        [os.environ["SOLVER"], "2", str(squarings % 100), modulo],
         stdout=subprocess.PIPE,
-        check=True,
     )
     base = proc.stdout.decode().strip()
     for _ in Bar("Solving time-lock puzzle").iter(range(100)):
         proc = subprocess.run(
-            [os.environ["SOLVER"], base, str(iterations // 100), modulo],
+            [os.environ["SOLVER"], base, str(squarings // 100), modulo],
             stdout=subprocess.PIPE,
-            check=True,
         )
         base = proc.stdout.decode().strip()
     secret_key = int(base)
@@ -61,17 +60,17 @@ def decrypt(encrypted_message: bytes, secret_key: int) -> bytes:
     return decrypted_message
 
 
-def encrypt_file(filename: str, iterations: int) -> None:
+def lock_file(filename: str, squarings: int) -> None:
     with open(filename, "rb") as file:
         message = file.read()
-    secret_key, modulo = gen_puzzle(iterations)
+    secret_key, modulo = gen_puzzle(squarings)
     encrypted_message = encrypt(message, secret_key)
     with open(f"{filename}.enc", "w") as enc:
         enc.write(
             "\n".join(
                 f"{k} = {v}"
                 for k, v in dict(
-                    t=iterations,
+                    t=squarings,
                     n=modulo,
                     msg=base64.b64encode(encrypted_message).decode(),
                 ).items()
@@ -80,26 +79,36 @@ def encrypt_file(filename: str, iterations: int) -> None:
         enc.write("\n")
 
 
-def decrypt_file(filename: str) -> None:
+def unlock_file(filename: str) -> None:
     with open(filename, "r") as dec:
         puzzle = {
             k: v.strip()
             for k, v in (line.split(" = ") for line in dec.readlines() if " = " in line)
         }
-    iterations, modulo, encrypted_message = (
+    squarings, modulo, encrypted_message = (
         int(puzzle["t"]),
         puzzle["n"],
         base64.b64decode(puzzle["msg"]),
     )
-    secret_key = solve_puzzle(iterations, modulo)
+    secret_key = solve_puzzle(squarings, modulo)
     decrypted_message = decrypt(encrypted_message, secret_key)
     with open(f"{filename}.dec", "wb") as file:
         file.write(decrypted_message)
 
 
+def benchmark_ns(squarings: str) -> int:
+    modulo = str(gen_puzzle(0)[1])
+    start = time.perf_counter_ns()
+    subprocess.run(
+        [os.environ["SOLVER"], "2", squarings, modulo],
+        stdout=subprocess.DEVNULL,
+    )
+    return time.perf_counter_ns() - start
+
+
 def usage() -> None:
-    print("Usage: timelock encrypt <iterations> <filename>")
-    print("       timelock decrypt <filename>")
+    print("Usage: timelock lock <squarings>|<seconds>s <filename>")
+    print("       timelock unlock <filename>")
     sys.exit(1)
 
 
@@ -107,21 +116,31 @@ def main() -> None:
     if len(sys.argv) < 2:
         usage()
     match sys.argv[1]:
-        case "encrypt":
+        case "lock":
             if len(sys.argv) != 4:
                 usage()
-            iterations = int(sys.argv[2])
+            if sys.argv[2].endswith("s"):
+                print("Running benchmark")
+                test_squarings = 10000000
+                elapsed_ns = benchmark_ns(str(test_squarings))
+                squarings = test_squarings * int(sys.argv[2][:-1]) * 10**9 // elapsed_ns
+                rate = test_squarings * 10**9 // elapsed_ns
+                print(
+                    f"{squarings} squarings take approximately {sys.argv[2]} with rate {rate}/s"
+                )
+            else:
+                squarings = int(sys.argv[2])
             filename = sys.argv[3]
-            print(f"Encrypting {filename}")
-            encrypt_file(filename, iterations)
-            print(f"Encrypted message written to {filename}.enc")
-        case "decrypt":
+            print(f"Locking {filename}")
+            lock_file(filename, squarings)
+            print(f"Time-locked message written to {filename}.enc")
+        case "unlock":
             if len(sys.argv) != 3:
                 usage()
             filename = sys.argv[2]
-            print(f"Decrypting {filename}")
-            decrypt_file(filename)
-            print(f"Decrypted message written to {filename}.dec")
+            print(f"Unlocking {filename}")
+            unlock_file(filename)
+            print(f"Unlocked message written to {filename}.dec")
         case _:
             usage()
 
